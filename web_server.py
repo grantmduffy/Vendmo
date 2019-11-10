b24ad9f3ee8426d161cac1fb7e9636b8fd01f156
Grant Duffy <grantd@buchanan-a.com>
2019-11-09 17:24:49 -0800

    Added reboot link to home page

--- web_server.py
+++ web_server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import subprocess
+from sys import platform
 
 app = Flask(__name__)
 
@@ -8,5 +10,16 @@ def home():
     return render_template('web_interface.html')
 
 
+@app.route('/reboot')
+def reboot():
+    if platform == 'linux':
+        # TODO: Schedule reboot and do cleanup
+        subprocess.call('sudo reboot')
+    else:
+        print('Reboot Raspberry Pi...')
+    return '<h1>Rebooting Raspberry Pi...</h1>' \
+           '<a href="/">home</a>'
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
