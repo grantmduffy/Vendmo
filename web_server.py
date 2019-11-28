36cda4c9672d0d02b49c21d9ca8246227043a488
Grant Duffy <grantd@buchanan-a.com>
2019-11-28 10:29:03 -0800

    Created settings class

--- web_server.py
+++ web_server.py
@@ -6,32 +6,44 @@ from wtforms.validators import InputRequired, Email, Length
 from subprocess import Popen
 from sys import platform
 from werkzeug.security import generate_password_hash, check_password_hash
+from json import dumps, loads
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shhh! this is secret!'
 Bootstrap(app)
 
-if platform == 'linux':
-    files_folder = '/home/pi/'
-else:
-    files_folder = ''
-password_file = files_folder + 'password.txt'
 
+class Settings:
 
-def get_password():
-    try:
-        with open(password_file, 'r') as f:
-            return f.read()
-    except FileNotFoundError:
-        with open(password_file, 'w') as f:
-            f.write(generate_password_hash('Vendmo', method='sha256'))
-        return get_password()
+    defaults = {
+        'password': generate_password_hash('Vendmo', method='sha256'),
+        'email_address': '',
+        'email_password': ''
+    }
+    file = 'settings.json'
 
+    def __init__(self):
+        if platform == 'linux':
+            self.file = '/home/pi/' + self.file
 
-def set_password(hashed_password):
-    with open(password_file, 'w') as f:
-        f.write(hashed_password)
+    def _load(self):
+        try:
+            with open(self.file, 'r') as f:
+                return loads(f.read())
+        except FileNotFoundError:
+            with open(self.file, 'w') as f:
+                f.write(dumps(self.defaults))
+            return self._load()
+
+    def __getattr__(self, item):
+        return self._load()[item]
+
+    def __setattr__(self, key, value):
+        s = self._load()
+        s[key] = value
+        with open(self.file, 'w') as f:
+            f.write(dumps(s))
 
 
 class LoginForm(FlaskForm):
@@ -63,17 +75,18 @@ def reboot():
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
+    s = Settings()
     login_form = LoginForm()
     settings_form = SettingsForm()
 
     if login_form.validate_on_submit():
-        if login_form.password.data and check_password_hash(get_password(), login_form.password.data):
+        if login_form.password.data and check_password_hash(s.password, login_form.password.data):
             return render_template('settings.html', form=settings_form)
 
     if settings_form.validate_on_submit():
         if settings_form.old_password.data and settings_form.new_password.data and\
-                check_password_hash(get_password(), settings_form.old_password.data):
-            set_password(generate_password_hash(settings_form.new_password.data, method='sha256'))
+                check_password_hash(s.password, settings_form.old_password.data):
+            s.password = generate_password_hash(settings_form.new_password.data, method='sha256')
         return render_template('settings.html', form=settings_form)
 
     return render_template('login.html', form=login_form)
