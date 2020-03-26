e2ec3852e327fe446475674dd38e9c2765516f9e
Grant Duffy <grantd@buchanan-a.com>
2020-03-25 21:23:51 -0700

    Added font and update method

--- display.py
+++ display.py
@@ -4,16 +4,21 @@ from RPi import GPIO
 from PIL import Image, ImageFont, ImageDraw
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-from PIL import ImageDraw
 
 DC = 24
 RST = 25
 LED = 15
 
+font = ImageFont.truetype('Roboto-Regular.ttf', size=30)
+
 TFT = TFT24T(spidev.SpiDev(), GPIO, landscape=True)
 TFT.initLCD(DC, RST, LED)
-TFT.clear((0, 0, 0))
+TFT.clear()
 
 d = TFT.draw()
-d.text((10, 10), 'Hello World', fill=255)
-TFT.display()
+
+
+def update(text):
+    TFT.clear()
+    d.text((10, 10), text, fill=(255, 255, 255), font=font)
+    TFT.display()
