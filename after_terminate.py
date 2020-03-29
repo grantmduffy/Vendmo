6dfe68cf4d57e6bcca9762306486a685367affdc
Grant Duffy <grantd@buchanan-a.com>
2020-03-29 00:43:58 -0700

    Fixed bugs with logging

--- after_terminate.py
+++ after_terminate.py
@@ -1,6 +1,6 @@
 from display import show_error
 
-with open('main_output.log' 'w') as f:
-    text = f.read()
+with open('main_output.log', 'rb') as f:
+    text = f.read().decode('latin-1')
 
 show_error(text)
